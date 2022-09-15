6ce3482dfb3af071e0ab6b608818addb4204b24e
Sebastian18 <atlasjuanse12@gmail.com>
2022-09-15 00:16:08 -0500

    tipos de datos en java

---
+++ src/Tarea1/tipodeDatos.java
@@ -0,0 +1,22 @@
+package Tarea1;
+
+public class tipodeDatos {
+    public static void main(String[] args) {
+       int numero = 10;
+       float decimal = 10.5f;
+       double decimal2 = 10.5;
+       char letra = 'a';
+       boolean verdadero = true;
+       String cadena = "Hola";
+       long  enteroLargo = 10000000;
+
+
+         System.out.println("El valor de la variable numero es: " + numero);
+         System.out.println("El valor de la variable decimal es: " + decimal);
+         System.out.println("El valor de la variable decimal2 es: " + decimal2);
+         System.out.println("El valor de la variable letra es: " + letra);
+         System.out.println("El valor de la variable boleana es: " + verdadero);
+         System.out.println("El valor de la variable cadena es: " + cadena);
+         System.out.println("El valor de la variable long es: " + enteroLargo);
+    }
+}
